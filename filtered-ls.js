92c72f3d25176cc9acdae91cb6315017ee145d69
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2015-12-11 14:27:29 +0500

    Filtered ls completed

---
+++ filtered-ls.js
@@ -0,0 +1,16 @@
+/**
+ * Filtered ls.
+ * fs && path module
+ */
+var fs = require('fs');
+var path = require('path');
+
+fs.readdir(process.argv[2], function(err, files) {
+  if (err) console.error(err);
+  for (var i = 0; i < files.length; i++) {
+    var extname = path.extname(files[i]);
+    if (extname == '.' + process.argv[3]) {
+      console.log(files[i]);
+    }
+  }
+});
